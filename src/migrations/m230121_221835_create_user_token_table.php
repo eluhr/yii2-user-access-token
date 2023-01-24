471087bdc1241bf463b1c528175182320b46a6bf
Elias Luhr <e.luhr@herzogkommunikation.de>
2023-01-24 14:07:03 +0100

    Update m230121_221835_create_user_token_table.php

--- src/migrations/m230121_221835_create_user_token_table.php
+++ src/migrations/m230121_221835_create_user_token_table.php
@@ -3,7 +3,7 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `{{%user_token}}`.
+ * Handles the creation of table `{{%user_access_token}}`.
  */
 class m230121_221835_create_user_token_table extends Migration
 {
@@ -12,7 +12,7 @@ class m230121_221835_create_user_token_table extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('{{%user_auth_token}}', [
+        $this->createTable('{{%user_access_token}}', [
             'id' => $this->primaryKey(),
             'token' => $this->text()->notNull(),
             'user_id' => $this->string()->notNull(),
@@ -26,6 +26,6 @@ class m230121_221835_create_user_token_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropTable('{{%user_auth_token}}');
+        $this->dropTable('{{%user_access_token}}');
     }
 }
